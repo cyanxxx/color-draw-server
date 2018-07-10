f8d52287d5c579abf1efa996e78dca124e1527f2
Your Name <you@example.com>
2018-07-10 21:12:15 +0800

    update .gitignore

--- db/credentials.js
+++
@@ -1,8 +0,0 @@
-var credentials = {
-  mongo: {
-      development: {
-        connectionString: 'mongodb://abc:abc123456@ds249128.mlab.com:49128/colordraw-db'
-      }
-  }
-}
-module.exports = credentials
