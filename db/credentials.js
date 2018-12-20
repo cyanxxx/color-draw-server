3e6b9a664f153a4a56888120b28204e9822766c4
cyanxxx <504512494@qq.com>
2018-12-20 13:53:43 +0800

    update .gitignore

--- db/credentials.js
+++
@@ -1,8 +0,0 @@
-var credentials = {
-  mongo: {
-      development: {
-        connectionString: 'mongodb://cyanxxx:cyan970612@ds249128.mlab.com:49128/colordraw-db'
-      }
-  }
-}
-module.exports = credentials
